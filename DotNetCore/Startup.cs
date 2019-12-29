c56cc74671700b06cbc6d2d4c4c15c49d14b9cc6
Michael McDonald <8325692+mcm-ham@users.noreply.github.com>
2019-12-29 11:33:45 +0800

    Add missing razor configuration

--- DotNetCore/Startup.cs
+++ DotNetCore/Startup.cs
@@ -23,6 +23,7 @@ namespace DotNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddRazorPages();
             services.AddControllersWithViews();
         }
 
@@ -39,15 +40,15 @@ namespace DotNetCore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
